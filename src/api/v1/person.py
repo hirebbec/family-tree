2f65d410b3784e7b19ffa1518d8c1a7e6f3ef76c
reek8 <reek867@gmail.com>
2024-12-07 23:59:24 +0300

    Fix order

--- src/api/v1/person.py
+++ src/api/v1/person.py
@@ -9,16 +9,16 @@ from services.person import PersonService
 router = APIRouter(prefix="/person", tags=["Persons"])
 
 
-@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=GetPersonSchema)
-async def get_person_by_id(id: int, person_service: PersonService = Depends()) -> GetPersonSchema:
-    return await person_service.get_person_by_id(id=id)
-
-
 @router.get("/all", status_code=status.HTTP_200_OK, response_model=Sequence[GetPersonSchema])
 async def get_all_persons(
     person_service: PersonService = Depends(),
 ) -> Sequence[GetPersonSchema]:
     return await person_service.get_all_persons()
+
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=GetPersonSchema)
+async def get_person_by_id(id: int, person_service: PersonService = Depends()) -> GetPersonSchema:
+    return await person_service.get_person_by_id(id=id)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=None)
